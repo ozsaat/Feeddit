2404f4218977bc2c08d1baa23f32f429e74a6588
Oz <oss202@gmail.com>
2015-03-13 20:23:36 +0000

    Changes

--- app/src/main/java/com/ozsaat/feeddit/NavigationAdapter.java
+++ app/src/main/java/com/ozsaat/feeddit/NavigationAdapter.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.List;
 
 public class NavigationAdapter extends RecyclerView.Adapter<NavigationAdapter.ViewHolder> {
-    private final ArrayList<String> items = new ArrayList<>();
+    private final ArrayList<NavigationItem> items = new ArrayList<>();
     private int feedlyItemsSize = 0;
     private int staticItemsSize = 0;
 
@@ -30,7 +30,7 @@ public class NavigationAdapter extends RecyclerView.Adapter<NavigationAdapter.Vi
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        String item = this.items.get(position);
+        NavigationItem item = this.items.get(position);
         holder.titleTextView.setText(item);
     }
 
@@ -48,9 +48,9 @@ public class NavigationAdapter extends RecyclerView.Adapter<NavigationAdapter.Vi
         }
     }
 
-    public void setFeedlyItems(Collection<? extends String> feedlyItems) {
-        List<? extends String> tempStaticList = new ArrayList<>(items.subList(0, staticItemsSize));
-        List<? extends String> tempRedditList = new ArrayList<>(items.subList(staticItemsSize+feedlyItemsSize, getItemCount()));
+    public void setFeedlyItems(Collection<NavigationItem> feedlyItems) {
+        List<NavigationItem> tempStaticList = new ArrayList<>(items.subList(0, staticItemsSize));
+        List<NavigationItem> tempRedditList = new ArrayList<>(items.subList(staticItemsSize+feedlyItemsSize, getItemCount()));
         items.clear();
         items.addAll(tempStaticList);
         if (feedlyItems != null) {
@@ -63,8 +63,8 @@ public class NavigationAdapter extends RecyclerView.Adapter<NavigationAdapter.Vi
         notifyDataSetChanged();
     }
 
-    public void setRedditItems(Collection<? extends String> redditItems) {
-        List<? extends String> tempList = new ArrayList<>(items.subList(0, staticItemsSize+feedlyItemsSize));
+    public void setRedditItems(Collection<NavigationItem> redditItems) {
+        List<NavigationItem> tempList = new ArrayList<>(items.subList(0, staticItemsSize+feedlyItemsSize));
         items.clear();
         items.addAll(tempList);
         if (redditItems != null) {
