5fcaf13486e1e3cfaa2ebc8de19cdd192579cf6d
Oz <oss202@gmail.com>
2015-05-11 19:48:33 +0100

    Added settings icon and played around with the layout

---
+++ app/src/main/java/com/ozsaat/feeddit/App.java
@@ -0,0 +1,15 @@
+package com.ozsaat.feeddit;
+
+import android.app.Application;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+public class App extends Application {
+
+    @Override public void onCreate() {
+        super.onCreate();
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder().setDefaultFontPath("fonts/Roboto-Regular.ttf")
+                                              .setFontAttrId(R.attr.fontPath)
+                                              .build());
+    }
+}
---
+++ app/src/main/java/com/ozsaat/feeddit/ui/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.ozsaat.feeddit.ui;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class BaseActivity extends AppCompatActivity {
+    @Override protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+}
